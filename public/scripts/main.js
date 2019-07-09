2306436a4ee94b03392a1cd540406ba9aee26c76
ElenaHongo <selenelunaw@hotmail.com>
2019-07-09 16:34:47 -0500

    fade in in titles completed

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,6 @@
 var deleteLog = false;
-var placeHolder = $('#callbacks-placeholder');
+var firstTitleShow = $('#callbacks-placeholder');
+var titleShow = $('.ani-title')
 
 $(document).ready(function(){
   $('#fullpage').fullpage({
@@ -8,22 +9,34 @@ $(document).ready(function(){
     continuousVertical: true,
     navigation: true,
     resetSliders: true,
-    
+    scrollingSpeed: 500,
     v2compatible: false,
     responsiveWidth: 600,
 
-    onLeave: function(origin, destination, direction) {
-      if (deleteLog) {
-        placeHolder.html('');
-      };
+   onLeave: function(origin, destination, direction) {
+      titleShow.hide(0);
+     },
+
+   onSlideLeave: function(section, origin, destination, direction) {
+      titleShow.hide(0);
     },
 
-    afterLoad: function(origin, destination, direction) {
-      if(origin){
-        placeHolder.append('<p>afterLoad</p>');
-      }
-      deleteLog = true;
+    afterSlideLoad: function(section, origin, destination, direction) {
+      titleShow.fadeIn(700);
     },
     
+   afterRender: function (){
+      titleShow.fadeIn(700);
+      setInterval(function(){
+        $.fn.fullpage.moveSectionDown();
+      }, 13000);
+    },
+
+   afterLoad: function(origin, destination, direction) {
+      if(origin){
+        titleShow.fadeIn(700);
+      }
+    },
+
   });
 });
