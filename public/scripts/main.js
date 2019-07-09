40f92dc894e1637714960f5a7600fc62141aaa50
Elena Hernandez Vazquez <selenelunaw@hotmail.com>
2019-07-09 09:22:34 -0500

    jquery compatible con fullpage

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,23 +1,29 @@
-/// <reference path="../../../../jquery/typings/globals/jquery/index.d.ts" />
+var deleteLog = false;
+var placeHolder = $('#callbacks-placeholder');
 
 $(document).ready(function(){
   $('#fullpage').fullpage({
     sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
-    scrollingSpeed: (400),
-    autoScrolling: true,
     scrollHorizontally: true,
     continuousVertical: true,
     navigation: true,
     resetSliders: true,
     
-    afterRender: function(){
-      setInterval(function(){
-        $.fn.fullpage.moveSectionDown(); 
-      }, 4000);
+    v2compatible: false,
+    responsiveWidth: 600,
+
+    onLeave: function(origin, destination, direction) {
+      if (deleteLog) {
+        placeHolder.html('');
+      };
     },
 
+    afterLoad: function(origin, destination, direction) {
+      if(origin){
+        placeHolder.append('<p>afterLoad</p>');
+      }
+      deleteLog = true;
+    },
+    
   });
-
- 
-
 });
